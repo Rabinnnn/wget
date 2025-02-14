617480c9a3246786e935299c8e8524c4cd99d0cc
svictor <victorshayo677@gmail.com>
2025-02-14 10:09:41 +0300

    feat: define Flags struct for storing download configurations

---
+++ config/flags.go
@@ -0,0 +1,16 @@
+package config
+
+import "flag"
+
+// Flags struct holds all the configurable parameters for the download operation.
+type Flags struct {
+    OutputFile   string 
+    OutputDir    string 
+    RateLimit    string 
+    Background   bool   
+    InputFile    string 
+    Mirror       bool   
+    Reject       string 
+    Exclude      string 
+    ConvertLinks bool   
+}
