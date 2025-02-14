14441c38fc89727b7baca5bf8b37de1f9f878055
svictor <victorshayo677@gmail.com>
2025-02-14 09:52:13 +0300

    feat: implement background mode by redirecting output to wget-log

--- main.go
+++ main.go
@@ -14,3 +14,15 @@ func main() {
     // Initialize flags and parse command-line arguments
     flags := config.InitFlags()
     flag.Parse()
+    // If background download flag is set, redirect output to a log file
+    if flags.Background {
+        logFile, err := os.Create("wget-log") // Create a log file
+        if err != nil {
+            fmt.Println("Error creating log file:", err)
+            return
+        }
+        defer logFile.Close()
+        os.Stdout = logFile // Redirect stdout to log file
+        os.Stderr = logFile // Redirect stderr to log file
+        fmt.Println("Output will be written to 'wget-log'.")
+    }
