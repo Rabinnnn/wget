10a557a81b0b1a291dab9dc18c0b1c6fbbb4d05d
Brace1000 <brianmoraro500@gmail..com>
2025-02-17 13:13:53 +0300

    feat:Enforce flags implementation

--- config/flags.go
+++ config/flags.go
@@ -1,7 +1,5 @@
 package config
-
 import "flag"
-
 // Flags struct holds all the configurable parameters for the download operation.
 type Flags struct {
     OutputFile   string 
@@ -14,22 +12,21 @@ type Flags struct {
     Exclude      string 
     ConvertLinks bool   
 }
-
 // InitFlags initializes and parses command-line flags.
 func InitFlags() *Flags {
     flags := &Flags{}
+    
+    // Initialize flags with their default values and descriptions
+    flag.StringVar(&flags.OutputFile, "O", "", "Save the file with a different name")
+    flag.StringVar(&flags.OutputDir, "P", ".", "Save the file in a specific directory")
+    flag.StringVar(&flags.RateLimit, "rate-limit", "", "Limit the download speed (e.g., 200k, 2M)")
+    flag.BoolVar(&flags.Background, "B", false, "Download in the background")
+    flag.StringVar(&flags.InputFile, "i", "", "File containing multiple URLs to download")
+    flag.BoolVar(&flags.Mirror, "mirror", false, "Mirror a website")
+    flag.StringVar(&flags.Reject, "R", "", "Reject specific file types (e.g., jpg,gif)")
+    flag.StringVar(&flags.Exclude, "X", "", "Exclude specific directories (e.g., /js,/css)")
+    flag.BoolVar(&flags.ConvertLinks, "convert-links", false, "Convert links for offline viewing")
+    
+    return flags
+}
 
-	    // Initialize flags with their default values and descriptions
-		flag.StringVar(&flags.OutputFile, "O", "", "Save the file with a different name")
-		flag.StringVar(&flags.OutputDir, "P", ".", "Save the file in a specific directory")
-		flag.StringVar(&flags.RateLimit, "rate-limit", "", "Limit the download speed (e.g., 200k, 2M)")
-		flag.BoolVar(&flags.Background, "B", false, "Download in the background")
-		flag.StringVar(&flags.InputFile, "i", "", "File containing multiple URLs to download")
-		flag.BoolVar(&flags.Mirror, "mirror", false, "Mirror a website")
-		flag.StringVar(&flags.Reject, "R", "", "Reject specific file types (e.g., jpg,gif)")
-		flag.StringVar(&flags.Exclude, "X", "", "Exclude specific directories (e.g., /js,/css)")
-		flag.BoolVar(&flags.ConvertLinks, "convert-links", false, "Convert links for offline viewing")
-		
-		return flags
-	}
-	
